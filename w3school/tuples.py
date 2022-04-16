2006b24d833b25547fe071489ff3bc86eb1f5cbe
Vaishali Gupta <gvaishalimlk@gmail.com>
2022-04-16 11:42:48 +0530

    Update tuples.py

--- w3school/tuples.py
+++ w3school/tuples.py
@@ -1,14 +1,20 @@
-thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
-print(thistuple)
-print(type(thistuple))
+# thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thistuple)
+# print(type(thistuple))
 
 
-# tuple constructor
-thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
-print(thistuple)
+# # tuple constructor
+# thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thistuple)
 
 
-#Access Tuple Items
+# #Access Tuple Items (access tuple items by referring to the index number)
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[1])
+
+
+# Negative Indexing(means start from the end.)
 
 thistuple = ("apple", "banana", "cherry")
-print(thistuple[1])
+print(thistuple[-1])
