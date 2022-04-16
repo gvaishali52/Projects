28fbd6f21175168cdda4b4c82ee8daa509bc17d3
Vaishali Gupta <gvaishalimlk@gmail.com>
2022-04-16 11:33:34 +0530

    Update tuples.py

--- w3school/tuples.py
+++ w3school/tuples.py
@@ -6,3 +6,9 @@ print(type(thistuple))
 # tuple constructor
 thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
 print(thistuple)
+
+
+#Access Tuple Items
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
